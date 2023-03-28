f8b1886f0f28d9ea46b0dbbbe7199445d68d3358
pako <pako@ite.edu.mx>
2023-03-27 17:56:49 -0700

    mmm

--- lector.py
+++ lector.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from bs4 import BeautifulSoup
+import re
 
 dicc_html = {
     "!<DOCTYPE>": 0,
@@ -34,8 +35,17 @@ html_doc = """<html><head></head>
 <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
 <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
 and they lived at the bottom of a well.</p>
-
+<p>
+<a>
 <p class="story">...</p>
+"""
+
+"""
+1-> [html,10],head,/head
+2-> body
+3-> p,b,/b
+4-> p
+
 """
 def busca_token(etiqueta):
     pass
@@ -52,6 +62,19 @@ print(soup.title)
 #    print("Si tiene contenido el titulo")
 #else:
 #    print("No tiene titulo salte de aqui")
-
+count = 1
 for tag in soup.find_all():
-    print(f"-> {tag.name} -- {tag.text}")
+    print(f"{count}-> {tag.name}")
+    count = count + 1
+
+tags = re.findall(r'<[^>]+>',html_doc)
+for t in tags:
+    print(t)
+
+
+
+
+
+
+
+    
