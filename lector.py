8e7545a956a49687a90b02dcb627f188d2368315
Priscilla Hurtado <95463323+PriscillaHM@users.noreply.github.com>
2023-04-26 12:53:21 -0700

    Actualizacion y resolucion de errores pylint

--- lector.py
+++ lector.py
@@ -1,3 +1,41 @@
-import beat
+#!/usr/bin/env python
+import re
+from tokens import dicc_html
 
-print("hola")
+#0 -> abre
+#1 -> cierra
+#2 -> neutron
+#3 - > desconocido
+
+def token_isopen(token):
+    """Recibe un token y devuelve true si es una etiqueta abierta """
+    for key,value in dicc_html.items():
+        if value == token:
+            if token in [0,20]:
+                pass
+                return 2
+            if token == 34:
+                return 1
+            if '/' in key:
+                return 1
+        else:
+            return 0
+        return value
+with open("index.html", "r", encoding='utf-8-sig') as f:
+    html_doc = f.read()
+tokens = []
+tags = re.findall(r'<[^>]+>',html_doc)
+for t in tags:
+    s = t.split()
+    bandera = 1
+    for key,value in dicc_html.items():
+        if key in s[0]:
+            print(f"{s[0]}\t\t=> {value}")
+            bandera = 0
+            tokens.append(value)
+            break
+    if bandera:
+        print(f"{s[0]} \t\t=> -1")
+
+html_close = [0,31]
+print(tokens)
